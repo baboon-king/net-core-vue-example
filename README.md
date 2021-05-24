bbcce756bbad940b852a47e9f4c9bfe6199a21d5
root <86382516@qq.com>
2021-05-24 13:59:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+使用webpack
+
+<pre class="dark">
+npm run build
+</pre>
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        index: './src/js/index.js',
+        about: './src/js/about.js',
+    },
+    output: {
+        filename: 'js/[name]/[name].js'
+    }
+}
